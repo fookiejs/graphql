625d4382251928cc6d33373e579f7f1d4ac1ed7f
umudik <seyitumutbicer@gmail.com>
2023-05-04 00:53:28 +0300

    dev

--- packages/graphql/index.ts
+++ packages/graphql/index.ts
@@ -3,63 +3,70 @@ import * as lodash from "lodash"
 import { resolve_input } from "./utils/resolve_input"
 import { create_query_resolver } from "./utils/create_query_resolver"
 
-export function create(Fookie) {
-    let typeDefs = `
-  input string_filter {
-    equals: String
-    not_equals: String
-    in: [String]
-    not_in: [String]
-    like: String
-  }
+const filter_types = `
+input string_filter {
+  equals: String
+  not_equals: String
+  in: [String]
+  not_in: [String]
+  like: String
+}
 
-  input integer_filter {
-    equals: Int
-    not_equals: Int
-    in: [Int]
-    not_in: [Int]
-    gte: Int
-    gt: Int
-    lte: Int
-    lt: Int
-  }
+input integer_filter {
+  equals: Int
+  not_equals: Int
+  in: [Int]
+  not_in: [Int]
+  gte: Int
+  gt: Int
+  lte: Int
+  lt: Int
+}
 
-  input float_filter {
-    equals: Float
-    not_equals: Float
-    in: [Float]
-    not_in: [Float]
-    gte: Float
-    gt: Float
-    lte: Float
-    lt: Float
-  }
+input float_filter {
+  equals: Float
+  not_equals: Float
+  in: [Float]
+  not_in: [Float]
+  gte: Float
+  gt: Float
+  lte: Float
+  lt: Float
+}
 
-  input boolean_filter {
-    equals: Boolean
-  }
+input boolean_filter {
+  equals: Boolean
+}
 `
+
+export function create(Fookie) {
+    const typeDefs = {
+        input: {},
+        type: {},
+        Query: {},
+    }
+
     const resolvers = {
         Query: {},
     }
 
     for (const model of Fookie.Core.models) {
-        let type = ``
-        let input = ``
+        if (model.name === "Query") {
+            throw Error("Model name can not be 'Query' ")
+        }
+        let typeFields = {}
+        let inputFields = {}
+
         for (const field of lodash.keys(model.schema)) {
             const temp_type = resolve_type(Fookie, model.schema[field])
             const temp_input = resolve_input(temp_type)
+
             if (model.schema[field].relation) {
-                type += `
-        ${field}_entity: ${model.schema[field].relation.name}
-      `
+                typeFields[`${field}_entity`] = `${model.schema[field].relation.name}`
             }
-            type += `
-      ${field}: ${temp_type}
-    `
-            input += `
-      ${field}: ${temp_input}
-    `
+
+            typeFields[field] = temp_type
+            inputFields[field] = temp_input
 
             if (model.schema[field].relation) {
                 resolvers[model.name] = {
@@ -79,23 +86,64 @@ export function create(Fookie) {
                 }
             }
         }
-        typeDefs += `
-    input ${model.name}_payload{
-      ${input}
-    }
-    type ${model.name}{
-       ${type}
-    }
-    type Query{
-        ${model.name}(payload:${model.name}_payload): [${model.name}]
-    }
-    `
+
+        typeDefs.input[model.name + "_payload"] = inputFields
+        typeDefs.type[model.name] = typeFields
+        typeDefs.Query[model.name] = `${model.name}_payload`
 
         resolvers.Query[model.name] = create_query_resolver(Fookie, model)
     }
 
+    for (const model of Fookie.Core.models) {
+        for (const field of lodash.keys(model.schema)) {
+            if (model.schema[field].relation) {
+                typeDefs.type[model.schema[field].relation.name]["all_" + model.name] = `[${model.name}]`
+                if (!resolvers[model.schema[field].relation.name]) {
+                    resolvers[model.schema[field].relation.name] = {}
+                }
+                resolvers[model.schema[field].relation.name]["all_" + model.name] = async function (parent, a, b, c) {
+                    const response = await Fookie.Core.run({
+                        model: model,
+                        method: Fookie.Method.Read,
+                        query: {
+                            filter: {
+                                [field]: parent[model.database.pk],
+                            },
+                        },
+                    })
+                    console.log(a)
+                    return response.data
+                }
+            }
+        }
+    }
+
+    let result = ""
+
+    for (const category in typeDefs) {
+        if (category === "Query") {
+            result += "type Query {\n"
+            for (const typeName in typeDefs[category]) {
+                result += `  ${typeName}(payload: ${typeDefs[category][typeName]}): [${typeName}]\n`
+            }
+            result += "}\n\n"
+        } else {
+            for (const typeName in typeDefs[category]) {
+                result += `${category} ${typeName} {\n`
+
+                for (const field in typeDefs[category][typeName]) {
+                    result += `  ${field}: ${typeDefs[category][typeName][field]}\n`
+                }
+
+                result += "}\n\n"
+            }
+        }
+    }
     return {
-        typeDefs,
+        typeDefs: `
+        ${filter_types}
+        ${result}
+        `,
         resolvers,
     }
 }
